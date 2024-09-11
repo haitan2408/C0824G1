34db9b6807f307355ce6a4d3880669b9600984c6
haitan2408 <haitan28102408@gmail.com>
2024-09-11 10:59:01 +0700

    HaiTT up demo JS

---
+++ Module 1/ss6/demo/demo.js
@@ -0,0 +1,5 @@
+function like() {
+    document.getElementById('btn').style.backgroundColor='blue';
+    document.getElementById('btn').style.color='white';
+    document.getElementById('btn').innerText='Unlike'
+}
