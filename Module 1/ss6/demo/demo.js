a1adc346f5fece1e0fed6b5769414095b3440c06
haitan2408 <haitan28102408@gmail.com>
2024-09-16 10:32:37 +0700

    HaiTT up demo dieu kien

--- Module 1/ss6/demo/demo.js
+++ Module 1/ss6/demo/demo.js
@@ -1,5 +1,5 @@
 function like() {
-    document.getElementById('btn').style.backgroundColor='blue';
-    document.getElementById('btn').style.color='white';
-    document.getElementById('btn').innerText='Unlike'
+    document.getElementById('btn').style.backgroundColor = 'blue';
+    document.getElementById('btn').style.color = 'white';
+    document.getElementById('btn').innerText = 'Unlike';
 }
